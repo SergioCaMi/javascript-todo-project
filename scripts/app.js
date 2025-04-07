0113c19b071f2846fc13515bbb5c90e66493de87
SergioCaMi <sergiocami84@gmail.com>
2025-04-07 10:25:06 +0200

    punto 6

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
-// /** TODO: Viene del inglés: "to do", es decir, una tarea que falta por hacer */
     const todoForm = document.getElementById("todo-form");
+    const actionsForm = document.querySelector(".actions__container");
     const todoInput = todoForm.querySelector(".input");
     const todosContainer = document.getElementById("todo");
+    const checkHide = actionsForm.querySelector(".checkbox");
 
      //Anidamos eventos: los eventos anidados se crean y quedan latentes. Así consigo que el codigo quede ordenado dentro del evento que crea el nuevo item y todo lo relacionado con éste.
+
+     //Hasta que no entremos en el evento del botón, el resto de eventos no existen (no están creados).
 
     document.querySelector(".button").addEventListener("click", e=>{
         e.preventDefault();
@@ -22,6 +25,14 @@
             todoItem.remove();
         });
     
+        checkHide.addEventListener("change", ()=>{
+            const itemsCompleted = todoItem.querySelectorAll(".toDoText");
+            for (let item of itemsCompleted){
+                if (item.parentNode.classList.contains("completed")){
+                    item.parentNode.classList.toggle("list-item__hide");
+                }
+            } 
+        });
         todosContainer.appendChild(todoItem);
         todoInput.value = "";
     });
